a4e0118a4339581f15986d9a26a023fffe0851ac
Christian Tigre <christian@acatha.com>
2025-06-08 16:00:28 -0500

    reset instalacion

--- src/enum/enum.tsx
+++
@@ -1,1 +0,0 @@
-export { Routes as RoutesModulo } from './enumRoutes';
--- src/enum/enumRoutes.ts
+++
@@ -1,9 +0,0 @@
-export enum Routes {
-	BASE = '/',
-	LOGIN = '/login',
-	PASSWORD_RESET = '/reset-password',
-	REGISTER = '/register',
-	HOME = '/home',
-	SIGNUP = '/signup',
-	SETTINGS = '/settings',
-}
