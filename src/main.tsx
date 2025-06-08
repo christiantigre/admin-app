e9a7f35c109d66a241b42cc0dcb29d5c9012de39
Christian Tigre <christian@acatha.com>
2025-06-08 16:56:51 -0500

    add:context

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
+import { MaterialTailwindControllerProvider } from "@/context";
 import App from './App.tsx'
+import '../public/css/tailwind.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+     <BrowserRouter>
+      <ThemeProvider>
+  <MaterialTailwindControllerProvider>
+          <App />
+        </MaterialTailwindControllerProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
